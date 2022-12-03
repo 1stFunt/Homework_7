23a45916e8c14bec8e837405ae4c324312b4c80b
Funt <funt_88@mail.ru>
2022-12-03 15:19:29 +0300

    task 1 completed

---
+++ Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+double[,] numbers = new double[rows, columns];
+FillArray(numbers);
+PrintImage(numbers);
+void PrintImage(double[,] image) //Немного переписанный метод для вывода нужного изображения
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            Console.Write($"{image[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(double[,] matr) //Метод для случайного заполнения двумерного массива
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Convert.ToDouble(new Random().Next(-1000, 1000) / 10.0);
+        }
+    }
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
